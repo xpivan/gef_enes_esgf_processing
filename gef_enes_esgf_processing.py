5d33cb11dee87f4405df4208ff0d633cf81dc9fd
Christian Page <christian.page@cerfacs.fr>
2017-11-23 17:23:31 +0100

    Generalized input parameters for ESGF script.

--- gef_enes_esgf_processing.py
+++ gef_enes_esgf_processing.py
@@ -12,14 +12,9 @@ from icclim.util import callback
 import cwt
 import time
 
-def netcdf_processing(in_file,
-                      params_file,
-                      api_key,
-                      out_file
+def netcdf_processing(params_file,
+                      api_key
                       ):
-
-# Setup ESGF CWT WPS
-    wps = cwt.WPS('https://aims2.llnl.gov/wps/', api_key=api_key)
 
 # Extract parameters for processing
     with open(params_file) as data_file:    
@@ -30,6 +25,9 @@ def netcdf_processing(in_file,
     out_var_name = plist["function"]["out_var_name"]
     slice_mode = plist["function"]["slice_mode"]
     if slice_mode == "None": slice_mode = None
+    in_file = plist["function"]["in_file"]
+    out_file = plist["function"]["out_file"]
+    esgf_cwt_wps_url = plist["function"]["esgf_cwt_wps_url"]
     try:
         geo_domain_latmin = plist["function"]["geo_domain"]["latitude"]["min"]
     except KeyError:
@@ -59,32 +57,45 @@ def netcdf_processing(in_file,
         
     period = [datetime.datetime(yyyy_b,mm_b,dd_b), datetime.datetime(yyyy_e,mm_e,dd_e)]
 
-    # (122, 240, 480)
-    tas = cwt.Variable('http://aims3.llnl.gov/thredds/dodsC/cmip5_css02_data/cmip5/output1/CMCC/CMCC-CM/decadal2005/mon/atmos/Amon/r1i2p1/tas/1/tas_Amon_CMCC-CM_decadal2005_r1i2p1_200511-201512.nc', 'tas')
+    # Get variable from in_file
+    varproc = cwt.Variable(in_file, var_name)
 
     if geo_domain_latmin != -9999 and geo_domain_latmax != -9999 and geo_domain_lonmin != -9999 and geo_domain_lonmax != -9999:
+
+        # Setup ESGF CWT WPS
+        wps = cwt.WPS(esgf_cwt_wps_url, api_key=api_key)
+
+        # Setup domain
         d0 = cwt.Domain([
                 cwt.Dimension('time', 100, 300, cwt.INDICES),
                 cwt.Dimension('lat', geo_domain_latmin, geo_domain_latmax),
                 cwt.Dimension('lon', geo_domain_lonmin, geo_domain_lonmax),
                 ])
 
-        cwt.gridder.Gridder('esmf', 'linear', tas)
-        proc = wps.get_process('CDAT.subset')
-        wps.execute(proc, inputs=[tas], domain=d0)
+        # Select gridder
+        cwt.gridder.Gridder('esmf', 'linear', varproc)
 
+        # Select processor
+        proc = wps.get_process('CDAT.subset')
+
+        # Execute processing
+        wps.execute(proc, inputs=[varproc], domain=d0)
+
+        # Check status and wait for completion
         while proc.processing:
             print proc.status
     
             time.sleep(1)
     
+        # Output some feedback to user
         print proc.status
 
         print proc.output.uri
         print proc.output.var_name
 
+        # Set output information
         in_file = proc.output.uri
         var_name = proc.output.var_name
 
-# Launch processing using icclim
+    # Launch processing using icclim
     icclim.indice(user_indice=my_indice_params, in_files=in_file, var_name=var_name, slice_mode=slice_mode, base_period_time_range=period, out_unit='days', out_file=out_file, callback=callback.defaultCallback2)
